a32d88231759b0e59ddbae1f8d77d85823d0162a
enderstream00 <enderstream00@naver.com>
2025-06-02 23:08:42 +0900

    chore: 주석 정리

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from 'react-router-dom'
 import { Toaster } from 'sonner'
-// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const App = () => {
   return (
     <>
       <Toaster richColors={true} theme='dark' />
       <Outlet />
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </>
   )
 }
--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -1,19 +1,16 @@
 import { createRoot } from 'react-dom/client'
-import '@/index.css'
+import '@/globals.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/routes'
-import { RecoilRoot } from 'recoil'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { SocketProvider } from './components/SocketContext'
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
-  <RecoilRoot>
     <QueryClientProvider client={queryClient}>
       <SocketProvider url='https://www.undaied.com'>
         <RouterProvider router={router} />
       </SocketProvider>
     </QueryClientProvider>
-  </RecoilRoot>
 )
